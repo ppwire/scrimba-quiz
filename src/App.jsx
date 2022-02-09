51cb48cbaa2dd0cae5e507c0a5e114864a6518e9
Poom Muangthai <poom.mth@hotmail.com>
2022-02-09 22:00:57 +0700

    Project init with start page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import StartPage from  './components/StartPage'
+
+function App() {
+
+  return (
+    <div className="app">
+      <StartPage></StartPage>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/StartPage.jsx
@@ -0,0 +1,13 @@
+const StartPage = () => {
+   return (
+      <div className="start-container" >
+         <h1>Quizzical</h1>
+         <h2>A Simple random quiz from trivia API</h2>
+         <button className="start-btn">
+            <h3>Start</h3>
+         </button>
+      </div>
+   )
+}
+
+export default StartPage
